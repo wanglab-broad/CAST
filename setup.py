a8c8a4af1bd7c8c2f00c01b01ca1aacba905fc1d
ztang <ztang@login01.broadinstitute.org>
2023-06-22 21:57:23 -0400

    upload_1

--- setup.py
+++ setup.py
@@ -4,4 +4,23 @@ setup(
     name="CAST",
     version="0.3",
     packages=find_packages(),
+    install_requires=[
+        'torch',
+        'dgl',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+        'h5py',
+        'statsmodels',
+        'tqdm',
+        'geopandas',
+        'Rtree',
+        'scanpy',
+        'libpysal',
+        'ipython',
+        'jupyterlab',
+        'jupyter',
+        'numpy',
+        'pandas'
+    ]
 )
